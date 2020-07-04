8145802c4fd2011df10b32912ab9fff36ab97423
IvanHephaestus <gefestroyale@gmail.com>
2020-07-04 20:52:18 +0300

    Engine[0.6.3]: DisplayServer: set display_target to nullptr in constructor

--- engine/sources/servers/display_server.cpp
+++ engine/sources/servers/display_server.cpp
@@ -4,7 +4,8 @@
 DisplayServer * DisplayServer::smp_singleton = nullptr;
 
 DisplayServer::DisplayServer(std::mutex& mtx):
-    mutex(mtx)
+    mutex(mtx),
+    mp_display_target(nullptr)
 {
     if(smp_singleton==nullptr){
         smp_singleton=this;
@@ -23,11 +24,12 @@ DisplayServer::~DisplayServer(){
 
 void DisplayServer::init_window(sf::VideoMode mode, const char* p_title){
     std::lock_guard<std::mutex> lock(smp_singleton->mutex);
-    Info("DisplayServer: created new window");
-    if(smp_singleton->mp_display_target==nullptr)
+    if(smp_singleton->mp_display_target==nullptr){
+        Info("DisplayServer: created new window");
         smp_singleton->mp_display_target = new RenderWindow(mode,p_title);
-    else
+    }else{
         Warning("DisplayServer: trying to init window twice");
+    }
 }  
 
 void DisplayServer::set_frame_rate_limit(int fps){
