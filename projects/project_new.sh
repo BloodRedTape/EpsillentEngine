bd0c332c762278ddd2d6b5acc448879b0d1b3889
IvanHephaestus <gefestroyale@gmail.com>
2020-07-05 09:43:18 +0300

    Engine[0.6.3]: Projects: implement windows build system autogeneration

--- projects/project_new.sh
+++ projects/project_new.sh
@@ -16,12 +16,13 @@ function create_project_structure {
     mkdir $1/out
     mkdir $1/sources
     mkdir $1/resources
-    info "Copying Engine resources"
+    info "Copying Game resources"
     cp -r ../engine/resources $1
+    cat ../engine/misc/Project_main > $1/sources/main.cpp
 }
 
-function generate_makefiles {
-    info "Configuring build system"
+function config_unix {
+    info "Configuring Unix build system"
     cp ../engine/misc/Project_Makefile $1/Makefile
     touch $1/ProjectConfig.mk
     echo "GAME_TITLE := $1" > $1/ProjectConfig.mk 
@@ -29,19 +30,12 @@ function generate_makefiles {
     cat ../engine/misc/Project_gitignore > $1/.gitignore
 }
 
-function example {
-    echo "Do you want to load an example application? (y/n)"
-    read answer
-    shopt -s extglob
-    if [ $answer = y ]
-    then
-        cat ../engine/misc/Project_main > $1/sources/main.cpp
-    elif [ $answer = n ]
-    then
-        echo
-    else
-        example $1
-    fi
+
+function config_windows {
+    info "Configuring Windows build system"
+    sed "s/TemplateProject/$1/g" ../engine/misc/Project_Solution > $1/$1.sln
+    sed "s/TemplateProject/$1/g" ../engine/misc/Project_vcxproj > $1/$1.vcxproj
+    cp ../engine/misc/Project_vcxproj_filters $1/$1.vcxproj.filters
 }
 
 if [ $# -ne 1 ] ; then
@@ -53,6 +47,6 @@ if [ -d "$1" ]; then
 fi
 
 create_project_structure "$1"
-generate_makefiles "$1"
-example "$1"
+config_unix "$1"
+config_windows "$1"
 info "Done"
