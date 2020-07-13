0a310aa90f3a0c408ed27b90a5499c658cb291ad
IvanHephaestus <gefestroyale@gmail.com>
2020-07-13 17:20:21 +0300

    [0.6.9] Math: vector angle func

--- engine/sources/core/math/math.cpp
+++ engine/sources/core/math/math.cpp
@@ -4,7 +4,16 @@ namespace Math{
 
 
 double vector_angle(const sf::Vector2f& vec){
-    return atan(vec.y/vec.x)/M_PI*180;
+    if(vec.x>=0 && vec.y >=0){
+        return 360.f-1.f*atan(vec.y/vec.x)/M_PI*180;
+    }else if(vec.y>= 0 && vec.x<0){
+        return -1.f*atan(vec.y/vec.x)/M_PI*180.f+180;
+    }else if(vec.x >= 0&& vec.y <0){
+        return 360.f-1.f*(360.f+atan(vec.y/vec.x)/M_PI*180.f);
+    }else{
+        return 360.f-1.f*(180.f+atan(vec.y/vec.x)/M_PI*180.f);
+    }
+
 }
 
-};
+}; // end of Math::
