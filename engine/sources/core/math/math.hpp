318da40101c75b79e901499a423877466a3a33a1
IvanHephaestus <gefestroyale@gmail.com>
2020-07-13 21:58:32 +0300

    [0.6.9] Math: few cool thing added

--- engine/sources/core/math/math.hpp
+++ engine/sources/core/math/math.hpp
@@ -4,14 +4,37 @@
 #include "core/math/random.hpp"
 #include "core/typedefs.hpp"
 #include <cmath>
+
+#define abs(x) ((x)>0?(x):(x)*-1)
+
 namespace Math{
 
 double vector_angle(const sf::Vector2f&);
 
-_ALWAYS_INLINE_ sf::Vector2f points_to_vector(const sf::Vector2f& begin,const sf::Vector2f& end){
+_FORCE_INLINE_ sf::Vector2f points_to_vector(const sf::Vector2f& begin,const sf::Vector2f& end){
     return sf::Vector2f(end.x-begin.x, end.y-begin.y);
 }
 
+_FORCE_INLINE_ sf::Vector2f normalize(const sf::Vector2f& vector){
+    sf::Vector2f absolute(abs(vector.x),abs(vector.y));
+    if(absolute.x==0.0f)
+        absolute.x=0.0001f;
+    if(absolute.y==0.0f)
+        absolute.y=0.0001f;
+    if(absolute.x>=absolute.y){
+        return sf::Vector2f(absolute.x/absolute.x*(vector.x/absolute.x),absolute.y/absolute.x*(vector.y/absolute.y));
+    }else{
+        return sf::Vector2f(absolute.x/absolute.y*(vector.x/absolute.x),absolute.y/absolute.y*(vector.y/absolute.y));
+    }
+}
+
+_FORCE_INLINE_ float degree(float rad){
+    return rad/M_PI*180;
+}
+
+_FORCE_INLINE_ float rad(float degree){
+    return degree/180*M_PI;
+}
 
 }; // end of Math::
 #endif
