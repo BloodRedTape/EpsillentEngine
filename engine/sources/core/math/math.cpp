2e2567d72b8c9b35e426aecc0642a7bd9cc364e5
Ivan Hephaestus <gefestroyale@gmail.com>
2020-07-23 17:12:46 -0400

    [0.7.0] fix windows build issues

--- engine/sources/core/math/math.cpp
+++ engine/sources/core/math/math.cpp
@@ -5,13 +5,13 @@ namespace Math{
 
 double vector_angle(const sf::Vector2f& vec){
     if(vec.x>=0 && vec.y >=0){
-        return atan(vec.y/vec.x)/M_PI*180;
+        return atan(vec.y/vec.x)/PI*180;
     }else if(vec.y>= 0 && vec.x<0){
-        return atan(vec.y/vec.x)/M_PI*180.f+180;
+        return atan(vec.y/vec.x)/PI*180.f+180;
     }else if(vec.x >= 0&& vec.y <0){
-        return 360.f+atan(vec.y/vec.x)/M_PI*180.f;
+        return 360.f+atan(vec.y/vec.x)/PI*180.f;
     }else{
-        return 180.f+atan(vec.y/vec.x)/M_PI*180.f;
+        return 180.f+atan(vec.y/vec.x)/PI*180.f;
     }
 }
 
