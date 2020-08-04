03aef8fab462ee138080e98f901ddb57adae5595
IvanHephaestus <gefestroyale@gmail.com>
2020-08-04 22:03:49 +0300

    [0.7.0]: Math: Vector Normalize Function

--- engine/sources/core/math/math.hpp
+++ engine/sources/core/math/math.hpp
@@ -15,18 +15,10 @@ double vector_angle(const sf::Vector2f&);
 _FORCE_INLINE_ sf::Vector2f points_to_vector(const sf::Vector2f& begin,const sf::Vector2f& end){
     return sf::Vector2f(end.x-begin.x, end.y-begin.y);
 }
-
-_FORCE_INLINE_ sf::Vector2f normalize(const sf::Vector2f& vector){
-    sf::Vector2f absolute(abs(vector.x),abs(vector.y));
-    if(absolute.x==0.0f)
-        absolute.x=0.0001f;
-    if(absolute.y==0.0f)
-        absolute.y=0.0001f;
-    if(absolute.x>=absolute.y){
-        return sf::Vector2f(absolute.x/absolute.x*(vector.x/absolute.x),absolute.y/absolute.x*(vector.y/absolute.y));
-    }else{
-        return sf::Vector2f(absolute.x/absolute.y*(vector.x/absolute.x),absolute.y/absolute.y*(vector.y/absolute.y));
-    }
+template <typename T>
+_FORCE_INLINE_ sf::Vector2<T> normalize(const sf::Vector2<T>& vector){
+    T length = std::sqrt(vector.x*vector.x+vector.y*vector.y); 
+    return sf::Vector2<T>(vector.x/length,vector.y/length);
 }
 
 _FORCE_INLINE_ float degree(float rad){
