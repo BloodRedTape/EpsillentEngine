68e2d0d6c2facd3a41c2faa036c951fd12eb79ed
IvanHephaestus <gefestroyale@gmail.com>
2020-08-13 22:02:19 +0300

    [0.7.7] Core: GUID: hash function

--- engine/sources/core/guid.hpp
+++ engine/sources/core/guid.hpp
@@ -35,7 +35,12 @@ namespace std
 	{
 		std::size_t operator()(GUID const &guid) const
 		{
-			return 2;
+			std::size_t hash = 0;
+            auto bytes = guid.bytes();
+            for(int i = 0; i< sizeof(GUID); i++){
+                hash += (long((bytes[i] << 4) | (bytes[i] >> 4)) | bytes[i]<<9)*0x934632;
+            }
+            return hash;
 		}
 	};
 }
