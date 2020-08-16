963e12bed74ca86ed5c3fd73570d2548579f71c9
IvanHephaestus <gefestroyale@gmail.com>
2020-08-16 12:52:59 +0300

    [0.7.8] ParticleSystem: approximate alpha color value

--- engine/sources/components/particle_system.cpp
+++ engine/sources/components/particle_system.cpp
@@ -16,7 +16,7 @@ Particle::Particle(const sf::Vector2f& position,const ParticleSystemProperties& 
 void Particle::on_introduce(){
     sprite = component_add<Sprite2D>();
     sprite->set_color(color_begin);
-    //sprite->set_color(sf::Color(color_begin.r + (color_begin.r-color_end.r)*(1-scale),color_begin.g + (color_begin.g-color_end.g)*(1-scale),color_begin.b + (color_begin.b-color_end.b)*(1-scale),255));
+    //sprite->set_color(sf::Color(color_begin.r - (color_begin.r-color_end.r)*(1-scale),color_begin.g - (color_begin.g-color_end.g)*(1-scale),color_begin.b - (color_begin.b-color_end.b)*(1-scale),color_begin.a - (color_begin.a-color_end.a)*(1-scale)));
     sprite->set_size(size);
     translate(rand()%dispersion.x-dispersion.x/2,rand()%dispersion.y-dispersion.y/2);
 }
@@ -26,7 +26,7 @@ void Particle::on_update(float dt){
     if(time>=life_time)destroy();
     scale = (life_time-time)/life_time;
     sprite->set_size(size*scale);
-    sprite->set_color(sf::Color(color_begin.r - (color_begin.r-color_end.r)*(1-scale),color_begin.g - (color_begin.g-color_end.g)*(1-scale),color_begin.b - (color_begin.b-color_end.b)*(1-scale),255));
+    sprite->set_color(sf::Color(color_begin.r - (color_begin.r-color_end.r)*(1-scale),color_begin.g - (color_begin.g-color_end.g)*(1-scale),color_begin.b - (color_begin.b-color_end.b)*(1-scale),color_begin.a - (color_begin.a-color_end.a)*(1-scale)));
     translate(direction*dt);
     rotate(delta_angle*dt);
 }
