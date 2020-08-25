9883551c7a8a96844ee6c7b43d9fae2a5b2a991a
IvanHephaestus <gefestroyale@gmail.com>
2020-08-25 21:50:56 +0300

    [0.8.3] ImageView: fix chaining

--- engine/sources/ui/image_view.cpp
+++ engine/sources/ui/image_view.cpp
@@ -10,10 +10,13 @@ void ImageView::on_render(){
 
 ImageView &ImageView::set_size(const sf::Vector2f &size){
     m_shape.setSize(size);
+    return *this;
 }
 ImageView &ImageView::set_position(const sf::Vector2f &position){
     m_shape.setPosition(position);
+    return *this;
 }
 ImageView &ImageView::set_texture(sf::Texture *texture){
     m_shape.setTexture(texture); 
+    return *this;
 }
